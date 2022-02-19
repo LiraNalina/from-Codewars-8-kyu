d54fdad854635626d0e3521a4324c04071e19d22
LiraNalina <48885835+LiraNalina@users.noreply.github.com>
2022-02-19 22:19:41 +0600

    Added index.js
    Added README.md

---
+++ index.js
@@ -0,0 +1,9 @@
+//Is n divisible by x and y?
+function isDivisible(n, x, y) {
+    if (n % x === 0 && n % y === 0 ) {
+      return true 
+    } else {
+      return false;
+    }
+  }
+  console.log(isDivisible(48, 3, 4))
