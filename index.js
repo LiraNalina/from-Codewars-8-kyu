aa744ca398f2634899fc31c30c35d2b16384ecb3
LiraNalina <48885835+LiraNalina@users.noreply.github.com>
2022-02-21 21:41:03 +0600

    New task from codewars

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ function isDivisible(n, x, y) {
     }
   }
   console.log(isDivisible(48, 3, 4))
+
+
+
+  //Find the remainder
+//Very tricky remainder for me
+function remainder(n, m) {
+  //Divide the larger argument by the smaller argument and return the remainder
+  if ((n === 0 && m < 0) || (m === 0 && n < 0) || (n < 0 && m < 0 && m === n)) {
+    return 0;
+  } else if (m === 0 || n === 0) {
+    return NaN;
+  } else if (n > m) {
+    return n % m;
+  } else if (m > n) {
+    return m % n;
+  } else if (n < 0 && n < 0 && m > n) {
+    return m;
+  } else if (n < 0 && n < 0 && n > m) {
+    return n;
+  }
+}
