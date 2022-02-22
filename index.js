6f59677a28c72790d9f38404233ebbc1e53d05cc
LiraNalina <48885835+LiraNalina@users.noreply.github.com>
2022-02-22 19:40:43 +0600

    New solved task from codewars

--- index.js
+++ index.js
@@ -39,3 +39,18 @@ function sumStr(a,b) {
 
 console.log(sumStr("-5", "3"))
 console.log(typeof(sumStr()))
+
+
+//You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+//If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+
+const areaOrPerimeter = function(l , w) {
+  // Return your answer
+if (l !== w) {
+  return (l + w) * 2
+} else {
+  return l * w
+}
+}
+console.log(areaOrPerimeter(7, 5))
